d61a4b6ec3617598ad38c977fa4d6f2e01d7600c
Geckota <tomorrow.is.another.day1829@gmail.com>
2018-09-30 21:33:34 +0900

    init

---
+++ app/src/main/java/com/websarva/wings/android/tamilycalendar/DailyCalendarActivity.java
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.tamilycalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DailyCalendarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_daily_calendar);
+    }
+}
